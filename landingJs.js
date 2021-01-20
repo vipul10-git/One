5617717430dd5c99c66c6f7d87e813a70f3acb7f
Vipul <vipul.singh10@outlook.com>
2021-01-20 19:46:38 +0530

    first

---
+++ landingJs.js
@@ -0,0 +1,28 @@
+const card = document.querySelector('#card');
+const container = document.querySelector('#container');
+const ripple = document.querySelector('#ripple');
+
+card.addEventListener('mousemove', (e)=>{
+    let x= (window.innerWidth/2- e.pageX)/10;
+    let y= (window.innerHeight/2- e.pageY)/10;
+    ripple.style.transform = `rotateY(${-x}deg) rotateX(${-y}deg)`
+    ripple.style.transform = 'translateZ(70px)';
+    card.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`
+});
+
+ripple.addEventListener('click', (e)=>{
+    console.log(window.innerWidth)
+    
+});
+ripple.addEventListener('mouseover',e=>{
+    card.style.transform = `rotateX(0deg) rotateY(0deg)`
+    ripple.style.transform = `rotateY(${0}deg) rotateX(${0}deg)`
+
+})
+
+card.addEventListener('mouseleave', (e)=>{
+    card.style.transform = `rotateX(0deg) rotateY(0deg)`
+    ripple.style.transform = `rotateY(${0}deg) rotateX(${0}deg)`
+    ripple.style.transform = 'translateZ(0px)';
+});
+
