1f45f920935fc28865e447105038e7b607611b95
Vipul <vipul.singh10@outlook.com>
2021-01-20 20:13:37 +0530

    css removed

--- landingJs.js
+++ landingJs.js
@@ -2,7 +2,7 @@ const card = document.querySelector('#card');
 const container = document.querySelector('#container');
 const ripple = document.querySelector('#ripple');
 
-card.addEventListener('mousemove', (e)=>{
+container.addEventListener('mousemove', (e)=>{
     let x= (window.innerWidth/2- e.pageX)/10;
     let y= (window.innerHeight/2- e.pageY)/10;
     ripple.style.transform = `rotateY(${-x}deg) rotateX(${-y}deg)`
@@ -20,7 +20,7 @@ ripple.addEventListener('mouseover',e=>{
 
 })
 
-card.addEventListener('mouseleave', (e)=>{
+container.addEventListener('mouseleave', (e)=>{
     card.style.transform = `rotateX(0deg) rotateY(0deg)`
     ripple.style.transform = `rotateY(${0}deg) rotateX(${0}deg)`
     ripple.style.transform = 'translateZ(0px)';
