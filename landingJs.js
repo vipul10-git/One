1be038a465d281e9034cb33e064cff1c40036a90
Vipul <vipul.singh10@outlook.com>
2021-01-21 14:43:21 +0530

    fix

--- landingJs.js
+++ landingJs.js
@@ -1,12 +1,15 @@
 const card = document.querySelector('#card');
 const container = document.querySelector('#container');
 const recod = document.querySelector('#recod');
-
+const ripple = document.querySelector('#ripple');
+const info = document.querySelector('#info');
+var mediaRecorder;
+var chunks =[];
 container.addEventListener('mousemove', (e)=>{
     let x= (window.innerWidth/2- e.pageX)/20;
     let y= (window.innerHeight/2- e.pageY)/20;
     recod.style.transform = `rotateY(${-x}deg) rotateX(${-y}deg)`
-    recod.style.transform = 'translateZ(70px)';
+    recod.style.transform = 'translateZ(100px)';
     card.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`
 });
 
@@ -15,11 +18,14 @@ recod.addEventListener('click', (e)=>{
     let x = setInterval(function() {
         --countDown;
         document.getElementById('info').innerHTML = '*'+countDown+' second left of recording';
+        ripple.classList.add("listen");
+        recod.style.transform = 'translateZ(100px)';
         if (countDown < 1) {
             clearInterval(x);
-            document.getElementById('info').innerHTML = '';
+            document.getElementById('info').innerHTML = 'Done';
         }
     }, 1000);
+
     navigator.mediaDevices.getUserMedia({ audio: true })
     .then(stream => {
         const mediaRecorder = new MediaRecorder(stream);
@@ -47,10 +53,12 @@ recod.addEventListener('click', (e)=>{
           });
 
         setTimeout(() => {
+            ripple.classList.remove("listen");
             mediaRecorder.stop();
           }, 8000);
     });
 });
+
 
 container.addEventListener('mouseleave', (e)=>{
     card.style.transform = `rotateX(0deg) rotateY(0deg)`
